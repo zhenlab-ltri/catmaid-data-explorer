e9dd92fbf19802d8f16c72766e85d3d2e704f5f8
d2fong <d2fong@uwaterloo.ca>
2020-11-17 16:49:54 -0500

    add datasets and neuron info to the model, add a compressed and uncompressed model output in the model generation python script

--- scripts/global_model/create_global_model.py
+++ scripts/global_model/create_global_model.py
@@ -2,28 +2,33 @@ import csv
 import json
 import os
 
+# this is the skeleton of the model
+# it will be filled out by the various functions below
+# and exported to json
 model = {
-    'datasets-sorted': ['SEM_L1_3', 'TEM_L1_5', 'SEM_L1_4', 'SEM_L1_2', 'SEM_L2_2', 'TEM_L3', 'TEM_adult', 'SEM_adult'],
-    'neurons': ['ADFL', 'ADFR', 'ADLL', 'ADLR', 'AFDL', 'AFDR', 'ALML', 'ALMR', 'ALNL', 'ALNR', 'AQR', 'ASEL', 'ASER', 'ASGL', 'ASGR', 'ASHL', 'ASHR', 'ASIL', 'ASIR', 'ASJL', 'ASJR', 'ASKL', 'ASKR', 'AUAL', 'AUAR', 'AVM', 'AWAL', 'AWAR', 'AWBL', 'AWBR', 'AWCL', 'AWCR', 'BAGL', 'BAGR', 'DVA', 'FLPL', 'FLPR', 'IL2DL', 'IL2DR', 'IL2L', 'IL2R', 'IL2VL', 'IL2VR', 'OLLL', 'OLLR', 'OLQDL', 'OLQDR', 'OLQVL', 'OLQVR', 'PLNL', 'PLNR', 'SAADL', 'SAADR', 'SAAVL', 'SAAVR', 'SDQL', 'SDQR', 'URBL', 'URBR', 'URXL', 'URXR', 'URYDL', 'URYDR', 'URYVL', 'URYVR', 'ADAL', 'ADAR', 'AIAL', 'AIAR', 'AIBL', 'AIBR', 'AINL', 'AINR', 'AIYL', 'AIYR', 'AIZL', 'AIZR', 'AVAL', 'AVAR', 'AVBL', 'AVBR', 'AVDL', 'AVDR', 'AVEL', 'AVER', 'BDUL', 'BDUR', 'DVC', 'PVCL', 'PVCR', 'PVNL', 'PVNR', 'PVPL', 'PVPR', 'PVR', 'PVT', 'RIAL', 'RIAR', 'RIBL', 'RIBR', 'RIFL', 'RIFR', 'RIGL', 'RIGR', 'RIH', 'RIML', 'RIMR', 'RIPL', 'RIPR', 'RIR', 'IL1DL', 'IL1DR', 'IL1L', 'IL1R', 'IL1VL', 'IL1VR', 'RIVL', 'RIVR', 'RMDDL', 'RMDDR', 'RMDL',
-                'RMDR', 'RMDVL', 'RMDVR', 'RMED', 'RMEL', 'RMER', 'RMEV', 'RMFL', 'RMFR', 'RMHL', 'RMHR', 'SIADL', 'SIADR', 'SIAVL', 'SIAVR', 'SIBDL', 'SIBDR', 'SIBVL', 'SIBVR', 'SMBDL', 'SMBDR', 'SMBVL', 'SMBVR', 'SMDDL', 'SMDDR', 'SMDVL', 'SMDVR', 'URADL', 'URADR', 'URAVL', 'URAVR', 'ADEL', 'ADER', 'AIML', 'AIMR', 'ALA', 'AVFL', 'AVFR', 'AVHL', 'AVHR', 'AVJL', 'AVJR', 'AVKL', 'AVKR', 'AVL', 'CEPDL', 'CEPDR', 'CEPVL', 'CEPVR', 'HSNL', 'HSNR', 'PVQL', 'PVQR', 'RICL', 'RICR', 'RID', 'RIS', 'RMGL', 'RMGR', 'BWM-DL01', 'BWM-DR01', 'BWM-VL01', 'BWM-VR01', 'BWM-DL02', 'BWM-DR02', 'BWM-VL02', 'BWM-VR02', 'BWM-DL03', 'BWM-DR03', 'BWM-VL03', 'BWM-VR03', 'BWM-DL04', 'BWM-DR04', 'BWM-VL04', 'BWM-VR04', 'BWM-DL05', 'BWM-DR05', 'BWM-VL05', 'BWM-VR05', 'BWM-DL06', 'BWM-DR06', 'BWM-VL06', 'BWM-VR06', 'BWM-DL07', 'BWM-DR07', 'BWM-VL07', 'BWM-VR07', 'BWM-DL08', 'BWM-DR08', 'BWM-VL08', 'BWM-VR08', 'CANL', 'CANR', 'CEPshDL', 'CEPshDR', 'CEPshVL', 'CEPshVR', 'GLRDL', 'GLRDR', 'GLRL', 'GLRR', 'GLRVL', 'GLRVR', 'excgl'],
+    'datasets': {},
+    'neurons': {},
+    'datasetsSorted': ['SEM_L1_3', 'TEM_L1_5', 'SEM_L1_4', 'SEM_L1_2', 'SEM_L2_2', 'TEM_L3', 'TEM_adult', 'SEM_adult'],
+    'neuronsSorted': ['ADFL', 'ADFR', 'ADLL', 'ADLR', 'AFDL', 'AFDR', 'ALML', 'ALMR', 'ALNL', 'ALNR', 'AQR', 'ASEL', 'ASER', 'ASGL', 'ASGR', 'ASHL', 'ASHR', 'ASIL', 'ASIR', 'ASJL', 'ASJR', 'ASKL', 'ASKR', 'AUAL', 'AUAR', 'AVM', 'AWAL', 'AWAR', 'AWBL', 'AWBR', 'AWCL', 'AWCR', 'BAGL', 'BAGR', 'DVA', 'FLPL', 'FLPR', 'IL2DL', 'IL2DR', 'IL2L', 'IL2R', 'IL2VL', 'IL2VR', 'OLLL', 'OLLR', 'OLQDL', 'OLQDR', 'OLQVL', 'OLQVR', 'PLNL', 'PLNR', 'SAADL', 'SAADR', 'SAAVL', 'SAAVR', 'SDQL', 'SDQR', 'URBL', 'URBR', 'URXL', 'URXR', 'URYDL', 'URYDR', 'URYVL', 'URYVR', 'ADAL', 'ADAR', 'AIAL', 'AIAR', 'AIBL', 'AIBR', 'AINL', 'AINR', 'AIYL', 'AIYR', 'AIZL', 'AIZR', 'AVAL', 'AVAR', 'AVBL', 'AVBR', 'AVDL', 'AVDR', 'AVEL', 'AVER', 'BDUL', 'BDUR', 'DVC', 'PVCL', 'PVCR', 'PVNL', 'PVNR', 'PVPL', 'PVPR', 'PVR', 'PVT', 'RIAL', 'RIAR', 'RIBL', 'RIBR', 'RIFL', 'RIFR', 'RIGL', 'RIGR', 'RIH', 'RIML', 'RIMR', 'RIPL', 'RIPR', 'RIR', 'IL1DL', 'IL1DR', 'IL1L', 'IL1R', 'IL1VL', 'IL1VR', 'RIVL', 'RIVR', 'RMDDL', 'RMDDR', 'RMDL',
+                      'RMDR', 'RMDVL', 'RMDVR', 'RMED', 'RMEL', 'RMER', 'RMEV', 'RMFL', 'RMFR', 'RMHL', 'RMHR', 'SIADL', 'SIADR', 'SIAVL', 'SIAVR', 'SIBDL', 'SIBDR', 'SIBVL', 'SIBVR', 'SMBDL', 'SMBDR', 'SMBVL', 'SMBVR', 'SMDDL', 'SMDDR', 'SMDVL', 'SMDVR', 'URADL', 'URADR', 'URAVL', 'URAVR', 'ADEL', 'ADER', 'AIML', 'AIMR', 'ALA', 'AVFL', 'AVFR', 'AVHL', 'AVHR', 'AVJL', 'AVJR', 'AVKL', 'AVKR', 'AVL', 'CEPDL', 'CEPDR', 'CEPVL', 'CEPVR', 'HSNL', 'HSNR', 'PVQL', 'PVQR', 'RICL', 'RICR', 'RID', 'RIS', 'RMGL', 'RMGR', 'BWM-DL01', 'BWM-DR01', 'BWM-VL01', 'BWM-VR01', 'BWM-DL02', 'BWM-DR02', 'BWM-VL02', 'BWM-VR02', 'BWM-DL03', 'BWM-DR03', 'BWM-VL03', 'BWM-VR03', 'BWM-DL04', 'BWM-DR04', 'BWM-VL04', 'BWM-VR04', 'BWM-DL05', 'BWM-DR05', 'BWM-VL05', 'BWM-VR05', 'BWM-DL06', 'BWM-DR06', 'BWM-VL06', 'BWM-VR06', 'BWM-DL07', 'BWM-DR07', 'BWM-VL07', 'BWM-VR07', 'BWM-DL08', 'BWM-DR08', 'BWM-VL08', 'BWM-VR08', 'CANL', 'CANR', 'CEPshDL', 'CEPshDR', 'CEPshVL', 'CEPshVR', 'GLRDL', 'GLRDR', 'GLRL', 'GLRR', 'GLRVL', 'GLRVR', 'excgl'],
     'stats': {
-        'max-contact-area': 0.0,
-        'max-connectivity-cs': 0,
-        'max-connectivity-gj': 0
+        'maxContactArea': 0.0,
+        'maxConnectivityCs': 0,
+        'maxConnectivityGj': 0
     },
-    'neuron-pair-data': {
+    'neuronPairData': {
     }
 }
 
 
 def init_model():
-    for pre in model['neurons']:
-        for post in model['neurons']:
+    for pre in model['neuronsSorted']:
+        for post in model['neuronsSorted']:
             key = neuron_pair_key(pre, post)
-            model['neuron-pair-data'][key] = {
+            model['neuronPairData'][key] = {
                 'contact': [],
-                'connectivity-cs': [],
-                'connectivity-gj': [],
+                'connectivityCs': [],
+                'connectivityGj': [],
                 'annotations': []
             }
 
@@ -31,19 +36,49 @@ def init_model():
 # row of number or None -- None means the dataset did not have a contact matrix file
 # if whole row is 0 or None set whole entry to None
 
-# connectivity documentation:
+# connectivity-(gj/cs) documentation:
 # row of number or None -- None means the dataset did not have a connectivity matrix file
 # if whole row is 0 or None set whole entry to None
+
+# annotations documentation:
+# annotations describe a connection
 
 
 def neuron_pair_key(neuron0, neuron1):
     return f'{neuron0}${neuron1}'
 
 
+def add_datasets_to_model():
+    with open('./scripts/global_model/datasets.json') as f:
+        model['datasets'] = json.load(f)
+
+
+def add_neuron_info_to_model():
+    neuron_info = None
+    with open('./scripts/global_model/neurons.json') as f:
+        neuron_info = json.load(f)
+
+    # TODO figure out how to interpret volume area for each neuron for each dataset
+    # neuron_volume_info = {}
+    # for dataset in model['datasetsSorted']:
+    #     fname = './scripts/global_model' + dataset + '_volume_area.csv'
+
+    for neuron in model['neuronsSorted']:
+        info = neuron_info['cells'][neuron]
+        class_members = neuron_info[info['class']]['classMembersIds']
+
+        model['neurons'][neuron] = {
+            'class': info['class'],
+            'types': info['types'],
+            'neurotransmitterTypes': info['neurotransmitterTypes'],
+            'classMembers': class_members
+        }
+
+
 def add_contact_area_to_model():
 
     max_area = 0.0
-    for dataset in model['datasets-sorted']:
+    for dataset in model['datasetsSorted']:
         fname = './scripts/global_model/' + dataset + '_adjacency.csv'
 
         if os.path.isfile(fname):
@@ -52,33 +87,33 @@ def add_contact_area_to_model():
                 for line in csvdata[1:]:
                     n0 = line[0]
                     for i in range(1, len(line)):
-                        n1 = model['neurons'][i-1]
+                        n1 = model['neuronsSorted'][i-1]
                         key = neuron_pair_key(n0, n1)
                         float_area = float(line[i])
 
                         if float_area > max_area:
                             max_area = float_area
 
-                        model['neuron-pair-data'][key]['contact'].append(
+                        model['neuronPairData'][key]['contact'].append(
                             float_area)
 
         else:
-            for n0 in model['neurons']:
-                for n1 in model['neurons']:
+            for n0 in model['neuronsSorted']:
+                for n1 in model['neuronsSorted']:
                     key = neuron_pair_key(n0, n1)
-                    model['neuron-pair-data'][key]['contact'].append(None)
+                    model['neuronPairData'][key]['contact'].append(None)
 
-    for k, v in model['neuron-pair-data'].items():
+    for k, v in model['neuronPairData'].items():
         s = sum(filter(None, v['contact']))  # filter None values from contact
         if s == 0.0:
-            model['neuron-pair-data'][k]['contact'] = None
+            model['neuronPairData'][k]['contact'] = None
 
-    model['stats']['max-contact-area'] = max_area
+    model['stats']['maxContactArea'] = max_area
 
 
 def add_connectivity_cs_to_model():
     max_connectivity = 0.0
-    for dataset in model['datasets-sorted']:
+    for dataset in model['datasetsSorted']:
         fname = './scripts/global_model/' + dataset + '_connectivity.csv'
 
         if os.path.isfile(fname):
@@ -87,35 +122,35 @@ def add_connectivity_cs_to_model():
                 for line in csvdata[1:]:
                     n0 = line[0]
                     for i in range(1, len(line)):
-                        n1 = model['neurons'][i-1]
+                        n1 = model['neuronsSorted'][i-1]
                         key = neuron_pair_key(n0, n1)
                         weight = int(line[i])
 
                         if weight > max_connectivity:
                             max_connectivity = weight
 
-                        model['neuron-pair-data'][key]['connectivity-cs'].append(
+                        model['neuronPairData'][key]['connectivityCs'].append(
                             weight)
 
         else:
-            for n0 in model['neurons']:
-                for n1 in model['neurons']:
+            for n0 in model['neuronsSorted']:
+                for n1 in model['neuronsSorted']:
                     key = neuron_pair_key(n0, n1)
-                    model['neuron-pair-data'][key]['connectivity-cs'].append(
+                    model['neuronPairData'][key]['connectivityCs'].append(
                         None)
 
-    for k, v in model['neuron-pair-data'].items():
+    for k, v in model['neuronPairData'].items():
         # filter None values from contact
-        s = sum(filter(None, v['connectivity-cs']))
+        s = sum(filter(None, v['connectivityCs']))
         if s == 0:
-            model['neuron-pair-data'][k]['connectivity-cs'] = None
+            model['neuronPairData'][k]['connectivityCs'] = None
 
-    model['stats']['max-connectivity-cs'] = max_connectivity
+    model['stats']['maxConnectivityCs'] = max_connectivity
 
 
 def add_connectivity_gj_to_model():
     max_connectivity = 0.0
-    for dataset in model['datasets-sorted']:
+    for dataset in model['datasetsSorted']:
         fname = './scripts/global_model/' + dataset + '_connectivity_gj.csv'
 
         if os.path.isfile(fname):
@@ -124,38 +159,38 @@ def add_connectivity_gj_to_model():
                 for line in csvdata[1:]:
                     n0 = line[0]
                     for i in range(1, len(line)):
-                        n1 = model['neurons'][i-1]
+                        n1 = model['neuronsSorted'][i-1]
                         key = neuron_pair_key(n0, n1)
                         weight = int(line[i])
 
                         if weight > max_connectivity:
                             max_connectivity = weight
 
-                        model['neuron-pair-data'][key]['connectivity-gj'].append(
+                        model['neuronPairData'][key]['connectivityGj'].append(
                             weight)
 
         else:
-            for n0 in model['neurons']:
-                for n1 in model['neurons']:
+            for n0 in model['neuronsSorted']:
+                for n1 in model['neuronsSorted']:
                     key = neuron_pair_key(n0, n1)
-                    model['neuron-pair-data'][key]['connectivity-gj'].append(
+                    model['neuronPairData'][key]['connectivityGj'].append(
                         None)
 
-    for k, v in model['neuron-pair-data'].items():
+    for k, v in model['neuronPairData'].items():
         # filter None values from contact
-        s = sum(filter(None, v['connectivity-gj']))
+        s = sum(filter(None, v['connectivityGj']))
         if s == 0:
-            model['neuron-pair-data'][k]['connectivity-gj'] = None
+            model['neuronPairData'][k]['connectivityGj'] = None
 
-    model['stats']['max-connectivity-gj'] = max_connectivity
+    model['stats']['maxConnectivityGj'] = max_connectivity
 
 
 def add_annotations_to_model():
     edge_classification_map = {
-        'increase': 'developmentally-added',
-        'decrease': 'developmentally-pruned',
+        'increase': 'developmentally added',
+        'decrease': 'developmentally pruned',
         'stable': 'stable',
-        'postembryonic': 'post-embryonic',
+        'postembryonic': 'post embryonic',
         'variable': 'variable'
     }
     with open('./scripts/global_model/edge_classifications.json') as f:
@@ -164,22 +199,25 @@ def add_annotations_to_model():
             for pair in v:
                 pre, post = pair
                 key = neuron_pair_key(pre, post)
-                model['neuron-pair-data'][key]['annotations'].append(
+                model['neuronPairData'][key]['annotations'].append(
                     edge_classification_map[k])
 
-    for pre in model['neurons']:
-        for post in model['neurons']:
+    for pre in model['neuronsSorted']:
+        for post in model['neuronsSorted']:
             key = neuron_pair_key(pre, post)
-            annotations = model['neuron-pair-data'][key]['annotations']
+            annotations = model['neuronPairData'][key]['annotations']
             if len(annotations) > 1:
                 print(annotations)
 
 
 init_model()
+add_datasets_to_model()
 add_contact_area_to_model()
 add_connectivity_cs_to_model()
 add_connectivity_gj_to_model()
 add_annotations_to_model()
 with open('./scripts/model-1.json', 'w') as f:
-    # json.dump(model, f, indent=2)
-    json.dump(model, f)
+    json.dump(model, f, indent=2)
+
+with open('./scripts/model-1.compressed.json', 'w') as f:
+    json.dump(model, f, separators=(',', ': '))
